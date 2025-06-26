c01f360430d9d7c78200c88dc2cc11161aa42e2a
Ticodev <umongegds@gmail.com>
2025-06-26 15:15:26 -0600

    Revert "Assistant checkpoint: Corregir errores de compilación en PermisosController"
    This reverts commit 70b23fcf440979575c79c913baa699b8a1990382.

--- Tuco.Clases/DTOs/SolicitudPermisoRequest.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Tuco.Clases.DTOs
-{
-    public class SolicitudPermisoRequest
-    {
-        public string Funcion { get; set; } = string.Empty;
-        public string Justificacion { get; set; } = string.Empty;
-    }
-}
