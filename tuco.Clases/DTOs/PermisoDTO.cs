be85cb5fe4eb2118311b187d276ab6bbcb171d4c
Ticodev <umongegds@gmail.com>
2025-06-26 14:41:51 -0600

    correccion

--- tuco.Clases/DTOs/PermisoDTO.cs
+++ tuco.Clases/DTOs/PermisoDTO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -22,6 +22,10 @@ namespace Tuco.Clases.DTOs
 
             [StringLength(255, ErrorMessage = "La descripción no puede exceder los 255 caracteres.")]
             public string? DescripcionPermiso { get; set; }
+
+
+            [StringLength(100, ErrorMessage = "La categoría no puede exceder los 100 caracteres.")]
+            public string? Categoria { get; set; }
         }
     }
 }
