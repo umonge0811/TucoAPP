e2f96339a9a6675be608e7799e739df0817fd842
Ticodev <umongegds@gmail.com>
2025-09-02 19:59:36 -0600

    Revert "Assistant checkpoint: Corregir inicialización del toggle de llanta"
    This reverts commit 58a2a701985da67be27b52b1edf17f394a3cdeab.

--- GestionLlantera.Web/wwwroot/js/views/inventario/agregarProducto.js
+++
@@ -1,7 +0,0 @@
-// Establecer el estado inicial como activado
-        if (esLlantaCheckbox && !esLlantaCheckbox.checked) {
-            esLlantaCheckbox.checked = true;
-        }
-
-        esLlantaCheckbox.addEventListener('change', actualizarTipoProducto);
-        actualizarTipoProducto(); // Inicializar estado
